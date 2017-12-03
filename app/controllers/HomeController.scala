ddd5232cdbd46160f3d465cd674a48293402ccf3
Eduardo Grajeda <tatofoo@gmail.com>
2017-12-02 22:37:22 -0300

    Remove comments from the seed template.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -14,10 +14,6 @@ import sangria.schema._
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, system: ActorSystem) extends AbstractController(cc) {
   // TODO: Investigate the relationship between this and the implicit ExecutionContext needed by the Executor.
@@ -100,13 +96,6 @@ class HomeController @Inject()(cc: ControllerComponents, system: ActorSystem) ex
     }
   """
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index() = Action.async { request =>
     QueryParser.parse(query) match {
       case Success(queryAst) =>
