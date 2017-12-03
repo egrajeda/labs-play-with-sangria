5a6b2f027550844584898113cd0521675a9d6e0d
Eduardo Grajeda <tatofoo@gmail.com>
2017-12-02 22:26:53 -0300

    Add the sample GraphQL schema and run a test query on it.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,15 +1,100 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
+import akka.actor.ActorSystem
 import play.api.mvc._
+import sangria.execution.{ErrorWithResolver, Executor, QueryAnalysisError}
+import sangria.macros._
+import sangria.macros.derive._
+import sangria.marshalling.playJson._
+import sangria.schema._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents, system: ActorSystem) extends AbstractController(cc) {
+  // TODO: Investigate the relationship between this and the implicit ExecutionContext needed by the Executor.
+  import system.dispatcher
+
+  case class Picture(width: Int, height: Int, url: Option[String])
+
+  // TODO: Investigate why the documentation puts this as implicit.
+  implicit val PictureType = deriveObjectType[Unit, Picture](
+    ObjectTypeDescription("The product picture."),
+    DocumentField("url", "Picture CDN URL.")
+  )
+
+  trait Identifiable {
+    def id: String
+  }
+
+  val IdentifiableType = InterfaceType(
+    "Identifiable",
+    "Entity that can be identified.",
+
+    fields[Unit, Identifiable](
+      Field("id", StringType, resolve = _.value.id)
+    )
+  )
+
+  case class Product(id: String, name: String, description: String) extends Identifiable {
+    def picture(size: Int): Picture =
+      Picture(width = size, height = size, url = Some(s"//cdn.com/$size/$id.jpg"))
+  }
+
+  val ProductType = deriveObjectType[Unit, Product](
+    Interfaces(IdentifiableType),
+    IncludeMethods("picture")
+  )
+
+  class ProductRepository {
+    private val Products = List(
+      Product("1", "Cheesecake", "Tasty"),
+      Product("2", "Health Potion", "+50 HP")
+    )
+
+    def product(id: String): Option[Product] = Products find { _.id == id }
+
+    def products: List[Product] = Products
+  }
+
+  val Id = Argument("id", StringType)
+
+  val QueryType = ObjectType(
+    "Query",
+
+    fields[ProductRepository, Unit](
+      Field("product", OptionType(ProductType),
+        description = Some("Returns a product with specific `id`."),
+        arguments = Id :: Nil,
+        resolve = c => c.ctx.product(c arg Id)),
+      Field("products", ListType(ProductType),
+        description = Some("Returns a list of all available products."),
+        resolve = _.ctx.products)
+    )
+  )
+
+  val schema = Schema(QueryType)
+
+  val query = graphql"""
+    query MyProduct {
+      product(id: "2") {
+        name
+        description
+
+        picture(size: 500) {
+          width, height, url
+        }
+      }
+
+      products {
+        name
+      }
+    }
+  """
 
   /**
    * Create an Action to render an HTML page.
@@ -18,7 +103,12 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def index() = Action.async { request =>
+    Executor.execute (schema, query, new ProductRepository)
+      .map (Ok(_))
+      .recover {
+        case error: QueryAnalysisError ⇒ BadRequest (error.resolveError)
+        case error: ErrorWithResolver ⇒ InternalServerError (error.resolveError)
+      }
   }
 }
--- build.sbt
+++ build.sbt
@@ -9,6 +9,8 @@ scalaVersion := "2.12.3"
 
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
+libraryDependencies += "org.sangria-graphql" %% "sangria" % "1.3.2"
+libraryDependencies += "org.sangria-graphql" %% "sangria-play-json" % "1.0.4"
 
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "com.egrajeda.controllers._"
